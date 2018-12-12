06282a5d72a9be52e18f6cb132d050b87f367652
BE4R <cid33@mail.ru>
2018-12-12 19:22:12 +0300

    Addad reset; fixed goto to not play immideatly

--- HTMLDevelpoment/site/input.js
+++ HTMLDevelpoment/site/input.js
@@ -2,17 +2,7 @@
         //массив названий видосов
         //массив превьюшек
 
-var vidar = [['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4']]/*, ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'],
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'],
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4'], 
-             ['test2.mp4', 'test1.mp4', 'test2.mp4'], ['test2.mp4', 'test1.mp4', 'test2.mp4']]*/
+var vidar = [['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'],['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4']]
+
+
 var imgar = [['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'],['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg'], ['1.jpg', '2.jpg', '1.jpg']]
