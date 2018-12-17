2c1ddc3a942029e7a54d730f5fe28ab062e559a3
BE4R <cid33@mail.ru>
2018-12-18 02:35:19 +0300

    Input updated

--- HTMLDevelpoment/site/input.js
+++ HTMLDevelpoment/site/input.js
@@ -1,6 +1,14 @@
     //должно прилететь:
+        //имя оригинала
         //массив названий видосов
         //массив превьюшек
+        //пути
+
+var workdir = '2'
+var viddir = '' //если пусто используется workdir
+var imgdir = '3' //если пусто используется workdir
+
+var origname = 'test1.mp4'
 
 var vidar = [['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'],['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4'], ['nope.mp4', 'ti.mp4', 'nariv.mp4']]
 
